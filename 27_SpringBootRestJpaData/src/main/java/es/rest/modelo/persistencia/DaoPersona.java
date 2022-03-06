52b9db8443688db2e331f923f81285d6cd3227f5
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-03-06 13:21:26 +0100

    add anotacion @Repository

--- 27_SpringBootRestJpaData/src/main/java/es/rest/modelo/persistencia/DaoPersona.java
+++ 27_SpringBootRestJpaData/src/main/java/es/rest/modelo/persistencia/DaoPersona.java
@@ -3,9 +3,11 @@ package es.rest.modelo.persistencia;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.rest.modelo.entidad.Persona;
 
+@Repository
 public interface DaoPersona extends JpaRepository<Persona, Integer>{
 
 	public List<Persona> findByNombre(String nombre);
