186311f70c58a0d5f340224b0013c8df0858ce6c
Felix de Pablo <f.depablo.lobo@gmail.com>
2023-01-25 19:34:40 +0100

    comentario evidente y poco productivo :)

--- 01_Basico/src/refactorizacion/_01_RefactorizacionCodigoOriginal.java
+++ 01_Basico/src/refactorizacion/_01_RefactorizacionCodigoOriginal.java
@@ -2,27 +2,27 @@ package refactorizacion;
 
 import java.util.Scanner;
 
-//Podemos mover el fichero a otra localización con "move" o arrastrando el
+//Podemos mover el fichero a otra localizaciï¿½n con "move" o arrastrando el
 //fichero
 
-//Podemos copiar el fichero a otra localización con "copy". Esta opción está al
-//pulsar botón derecho sobre el fichero. Se puede hacer tambien con "ctrl + C"
+//Podemos copiar el fichero a otra localizaciï¿½n con "copy". Esta opciï¿½n estï¿½ al
+//pulsar botï¿½n derecho sobre el fichero. Se puede hacer tambien con "ctrl + C"
 
-//Podemos pegar el fichero una vez compiado. Esta opción está al
-//pulsar botón derecho sobre el fichero. Se puede hacer tambien con "ctrl + V"
+//Podemos pegar el fichero una vez compiado. Esta opciï¿½n estï¿½ al
+//pulsar botï¿½n derecho sobre el fichero. Se puede hacer tambien con "ctrl + V"
 
 //Podemos cambiar el nombre de una clase haciendo un "rename" del fichero
 public class _01_RefactorizacionCodigoOriginal {
-
+	//MÃ©todo principal de la clase
 	public static void main(String[] args) {
 		
 		//Aqui vamos a convertir la cadena "******" a una constante, 
-		//mediante la opción "Extract constant"
+		//mediante la opciï¿½n "Extract constant"
 		System.out.println("******" + "Bienvenidos a mi programa" + "******");
 				
 		//Podemos extraer esta cadena a una variable, para ello 
 		//haremos un "Extract local variable". Podriamos hacer lo  mismo 
-		//si fuera un número u otro valor.
+		//si fuera un nï¿½mero u otro valor.
 		
 		//Una vez que hayamos extraido la cadena a una variable local, 
 		//haremos un refactor del nombre con "rename"		
@@ -50,8 +50,8 @@ public class _01_RefactorizacionCodigoOriginal {
 		return suma;
 	}
 	
-	//Extracion de metodos, podemos extraer parte de codigo a otros métodos
-	//mediante la opción "Extract method"
+	//Extracion de metodos, podemos extraer parte de codigo a otros mï¿½todos
+	//mediante la opciï¿½n "Extract method"
 	public static void operar(int operacion) {
 		if(operacion == 0) {
 			System.out.println("Damos alta el nombre, opcion elegida " + operacion);
