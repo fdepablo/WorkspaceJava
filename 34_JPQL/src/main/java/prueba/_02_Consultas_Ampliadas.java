0911dedd550966b9220f39f50815a4230013c379
Felix de Pablo <f.depablo.lobo@gmail.com>
2023-02-10 19:04:26 +0100

    jpql -> separados los ejemplos nuevos en otro main

--- 34_JPQL/src/main/java/prueba/_02_Consultas_Ampliadas.java
+++ 34_JPQL/src/main/java/prueba/_02_Consultas_Ampliadas.java
@@ -12,7 +12,7 @@ import javax.persistence.TypedQuery;
 import modelo.entidad.Cliente;
 import modelo.entidad.Direccion;
 
-public class _01_Consultas {
+public class _02_Consultas_Ampliadas {
 
 	public static EntityManagerFactory emf = null;
 
@@ -56,7 +56,43 @@ public class _01_Consultas {
 		for (Object[] p : resultados) {
 			System.out.println(p[0] + " - " + p[1]);//la posicion 0 tiene el nombre y la 1 el telefono
 		}
+		
+		//Desde aquí es lo que se ha agregado los nuevos ejemplos respecto al 01_Consultas
+		System.out.println("===================================");
+		query = em.createQuery("SELECT cli FROM Cliente cli WHERE cli.telefono = 555");
+		list = query.getResultList();
+		System.out.println("==== listado de clientes con telefono 555====");
+		listarClientes(list);
 				
+		System.out.println("===================================");
+		query = em.createQuery("SELECT cli FROM Cliente cli");
+		query.setFirstResult(0);//ojo, el id 1 es el primero
+		query.setMaxResults(3);//ojo, el máximo numero de resultados
+		list = query.getResultList();
+		System.out.println("==== listado de clientes con paginación del 0 y maximo 2 ====");
+		listarClientes(list);
+		
+		System.out.println("===================================");
+		//Esto es un ejemplo de inyección SQL, jamas se debe de concatenar un valor
+		//variable a una consulta SQL o JPQL
+		//El siguiente ejemplo esta MUY MAL. 
+		String cadena = "555";
+		query = em.createQuery("SELECT cli FROM Cliente cli WHERE cli.telefono = " + cadena);
+		Cliente clienteBurns = (Cliente)query.getSingleResult();//SOLO si da un único resultado
+		System.out.println("==== Cliente con telefono 555 ====");
+		System.out.println("Cliente-> nombre: " + clienteBurns.getNombre() + "; Telefono: " + clienteBurns.getTelefono());
+		
+		//La manera de usar datos introducidos por el cliente, siempre debe ser de manera
+		//parametrizada.
+		//Parametrizando datos de entrado podemos evitar inyecciones SQL
+		System.out.println("===================================");
+		cadena = "555";
+		query = em.createQuery("SELECT cli FROM Cliente cli WHERE cli.telefono = :telefono");
+		query.setParameter("telefono", cadena);
+		clienteBurns = (Cliente)query.getSingleResult();//SOLO si da un único resultado
+		System.out.println("==== Cliente con telefono 555 PARAMETRIZADO ====");
+		System.out.println("Cliente-> nombre: " + clienteBurns.getNombre() + "; Telefono: " + clienteBurns.getTelefono());
+		
 		System.out.println("===================================");
 		//Con TypedQuery podemos crear queries con valores genericos
 		//o parametrizados
@@ -97,6 +133,16 @@ public class _01_Consultas {
 		c = new Cliente(null, "El hijo de Burns", "666", null);
 		em.persist(c);
 		
+		//Homer, Bart y Lisa se han agregado nuevos
+		c = new Cliente(null, "Homer", "777", null);
+		em.persist(c);
+		
+		c = new Cliente(null, "Bart", "888", null);
+		em.persist(c);
+		
+		c = new Cliente(null, "Lisa", "999", null);
+		em.persist(c);
+
 		em.getTransaction().commit();
 		em.close();
 	}
