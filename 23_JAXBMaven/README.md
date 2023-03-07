79aa492f4b10a2a5c9633423290941948b71d631
Felix de Pablo <f.depablo.lobo@gmail.com>
2023-03-07 21:32:21 +0100

    Mejoras gramaticales, se agrego el atributo @XMLTransient

--- 23_JAXBMaven/README.md
+++ 23_JAXBMaven/README.md
@@ -1,13 +1,13 @@
 
 # JAXB
 
-**Java JAXB ** o Java XML API Binding nos permite trabajar con XML y JSON de una forma cómoda usando Java.
+**Java JAXB ** o Java XML API Binding nos permite trabajar con XML de una forma cómoda usando Java.
 
-Antes de **Java 11**, las librerías de JAXB estaban dentro de la JDK de Java, pero para esta versión las sacaron de ella, por lo que hay que descargarlas y agregarlas al classpath de nuestro proyecto.
+Antes de **Java 11**, las librerías de JAXB estaban dentro de la JDK de Java, pero a partir de esta versión, las sacaron de la JDK, por lo que hay que descargarlas y agregarlas al classpath de nuestro proyecto.
 
 Usaremos maven para bajarnos las librerías y agregar al classpath (ver ejemplo 30_Maven para usar maven)
 
-Si te da error este proyecto al importarlo hacer boton derecho sobre el proyecto -> Maven -> Update Maven Proyect para bajarte las librer�as que necesita este proyecto de internet
+Si te da error este proyecto al importarlo hacer boton derecho sobre el proyecto -> Maven -> Update Maven Proyect para bajarte las librerías que necesita este proyecto de internet
 
 ## Ayuda
 
--- 23_JAXBMaven/src/main/java/beans/Persona.java
+++ 23_JAXBMaven/src/main/java/beans/Persona.java
@@ -3,13 +3,14 @@ package beans;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 //Estas anotaciones sirven para que JAXB que es el motor de java para
 //convertir objetos a XML y a la inversa, sepa como hacerlo
 //serializar y deserializar
 
-//esta etiqueta estamos estableciendo el nombre de el nodo raiz en xml
+//Esta etiqueta estamos estableciendo el nombre de el nodo raiz en xml
 //etiqueta obligatoria
 @XmlRootElement(name="persona")
 //Podemos hacer que las etiquetas salgan en un determinado orden
@@ -48,10 +49,25 @@ public class Persona {
 	}
 	public void setIdPersona(int idPersona) {
 		this.idPersona = idPersona;
+	}	
+
+	//Etiqueta opcional, podemos usarla para cambiar el nombre de la etiqueta XML
+	@XmlElement(name="apellidos")
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public Direccion getDireccion() {
+		return direccion;
 	}
 
-	//etiqueta opcional
-	@XmlElement
+	//Si en algún momento no queremos serializar un atributo, podemos usar 
+	//la siguiente anotación
+	//@XmlTransient
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -59,16 +75,10 @@ public class Persona {
 		this.nombre = nombre;
 	}
 
-	@XmlElement(name="apellidos")
-	public String getApellido() {
-		return apellido;
-	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-	@XmlElement
 	public int getEdad() {
 		return edad;
 	}
@@ -77,19 +87,9 @@ public class Persona {
 		this.edad = edad;
 	}
 
-	public Direccion getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(Direccion direccion) {
-		this.direccion = direccion;
-	}
-
 	@Override
 	public String toString() {
 		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad
 				+ ", direccion=" + direccion + "]";
-	}
-	
-	
+	}	
 }
