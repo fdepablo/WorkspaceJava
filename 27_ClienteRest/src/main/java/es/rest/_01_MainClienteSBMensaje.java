828356aea11f838d5305888a8ed1e19c128bcd96
Felix de Pablo <f.depablo.lobo@gmail.com>
2023-12-29 13:06:26 +0100

    mejoras de código y documentación

--- 27_ClienteRest/src/main/java/es/rest/_01_MainClienteSBMensaje.java
+++ 27_ClienteRest/src/main/java/es/rest/_01_MainClienteSBMensaje.java
@@ -9,24 +9,30 @@ import java.time.Duration;
 
 public class _01_MainClienteSBMensaje {
 
-	private static final HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1)
-			.connectTimeout(Duration.ofSeconds(10)).build();
+	private static final HttpClient httpClient = HttpClient.newBuilder()
+			.version(HttpClient.Version.HTTP_1_1)
+			.connectTimeout(Duration.ofSeconds(10))
+			.build();
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-
-		HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/mensaje"))
-				.setHeader("User-Agent", "Java 11 HttpClient Bot")
+		//GET /mensaje http 1.1
+		//User-Agent: Java 17 HttpClient Felix S.L.
+		HttpRequest request = HttpRequest.newBuilder()
+				.GET().uri(URI.create("http://localhost:8080/mensaje"))
+				.setHeader("User-Agent", "Java 17 HttpClient Felix S.L.")
 				.build();
 
-		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+		HttpResponse<String> response = httpClient
+				.send(request, HttpResponse.BodyHandlers.ofString());
 
 		// print response headers
 		HttpHeaders headers = response.headers();
-		headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
+		//Recorremos las cabeceras que estan en un hashmap mediante Streams y funciones lambda
+		//headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
 
 		// print status code
 		System.out.println(response.statusCode());
-
+		
 		// print response body
 		System.out.println(response.body());
 
