401aadeefd6c7ca2ea707eb1fe2771048e336506
Felix de Pablo <f.depablo.lobo@gmail.com>
2023-12-29 13:08:07 +0100

    corrección de tildes

--- 19_SocketClient/src/cliente/SocketCliente.java
+++ 19_SocketClient/src/cliente/SocketCliente.java
@@ -19,26 +19,26 @@ public class SocketCliente {
 	public static final String IP_SERVER = "localhost";
 	
 	public static void main(String[] args) {
-		System.out.println("        APLICACIÓN CLIENTE         ");
+		System.out.println("        APLICACIï¿½N CLIENTE         ");
 		System.out.println("-----------------------------------");
 		
 		//Socket -> es la clase que nos va a permitir comunicarnos con el servidor
 
 		//InputStreamReader entrada -> Entrada de datos. Es el canal de entrada del 
 		//cliente, es decir, el canal por el cual el servidor nos va a mandar la 
-		//información.
+		//informaciï¿½n.
 
 		//PrintStream salida -> Salida de datos. Es el canal de salida del cliente, 
-		//es decir, el canal por el cual vamos a enviar información al servidor.		
+		//es decir, el canal por el cual vamos a enviar informaciï¿½n al servidor.		
 
 		//En este objeto vamos a encapsular la IP y el puerto al que nos vamos a conectar
 		InetSocketAddress direccionServidor = new InetSocketAddress(IP_SERVER, PUERTO);
 		
-		//Usaremos la declaracion try-with-resources, esta declaración nos asegura que
-		//todos los objetos que declaremos e instanciemos dentro del bloque try, serán
+		//Usaremos la declaracion try-with-resources, esta declaraciï¿½n nos asegura que
+		//todos los objetos que declaremos e instanciemos dentro del bloque try, serï¿½n
 		//cerrados cuando salgan del bloque try-catch. Es muy util parar ahorranos codigo
-		//ya que sino deberíamos cerrarlos (ejecutar su metodo close()), dentro del bloque
-		//finally, creando muchas más lineas para ello.
+		//ya que sino deberï¿½amos cerrarlos (ejecutar su metodo close()), dentro del bloque
+		//finally, creando muchas mï¿½s lineas para ello.
 		try (Scanner sc = new Scanner(System.in);
 			Socket socketAlServidor = new Socket()){
 					
@@ -47,24 +47,24 @@ public class SocketCliente {
 			String numero1 = sc.nextLine();//supongamos el numero 3
 			String numero2 = sc.nextLine();//supongamos el numero 4
 			
-			//En este caso voy a establecer que mi comunicación entre el cliente y el
+			//En este caso voy a establecer que mi comunicaciÃ³n entre el cliente y el
 			//servidor va a ser que le mando los dos numeros separados por un "-"
-			//para luego en el servidor obtener ambmos numeros mediante el 
+			//para luego en el servidor obtener ambos numeros mediante el 
 			//metodo split() de la clase String
 			String operandos = numero1 + "-" + numero2;//3-4
 			
 			//Observese como no conviertos los datos a enteros, ya que cuando
 			//se mandan a traves de un socket se mandan SIEMPRE en formato cadena
 			
-			//Establecemos la conexión
-			System.out.println("CLIENTE: Esperando a que el servidor acepte la conexión");
+			//Establecemos la conexiÃ³n
+			System.out.println("CLIENTE: Esperando a que el servidor acepte la conexiï¿½n");
 			socketAlServidor.connect(direccionServidor);			
 			System.out.println("CLIENTE: Conexion establecida... a " + IP_SERVER 
 					+ " por el puerto " + PUERTO);		
 			
 			//Creamos el objeto que nos permite mandar informacion al servidor
 			PrintStream salida = new PrintStream(socketAlServidor.getOutputStream());
-			//Mandamos la información por el Stream
+			//Mandamos la informaciï¿½n por el Stream
 			salida.println(operandos);//3-4
 			
 			//Creamos el objeto que nos va a permitir leer la salida del servidor
@@ -76,7 +76,7 @@ public class SocketCliente {
 			
 			System.out.println("CLIENTE: Esperando al resultado del servidor...");
 			//En la siguiente linea se va a quedar parado el hilo principal
-			//de ejecución hasta que el servidor responda, es decir haga un println
+			//de ejecuciï¿½n hasta que el servidor responda, es decir haga un println
 			String resultado = bf.readLine();//7
 				
 			System.out.println("CLIENTE: El resultado de la suma es: " + resultado);//7
@@ -84,7 +84,7 @@ public class SocketCliente {
 			//Nota, no cierro ni "entrada" ni "salida" ya que se encarga el objeto
 			//socket cuando salgamos del bloque try-catch
 		} catch (UnknownHostException e) {
-			System.err.println("CLIENTE: No encuentro el servidor en la dirección" + IP_SERVER);
+			System.err.println("CLIENTE: No encuentro el servidor en la direcciÃ³n" + IP_SERVER);
 			e.printStackTrace();
 		} catch (IOException e) {
 			System.err.println("CLIENTE: Error de entrada/salida");
